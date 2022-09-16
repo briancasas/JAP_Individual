a043f32dc096f9691b8daaafefa73507a199e2fb
Brian Correa <brian2010correa@hotmail.com>
2022-09-16 17:01:48 -0300

    arreglos varios

--- js/product-info.js
+++ js/product-info.js
@@ -1,7 +1,11 @@
 let infProductList = [];
-let infProductComents = {};
+let infProductComents = [];
 let photos = [];
 let infoP = localStorage.getItem("ProdID");
+let arrayComs = [];
+
+
+
 
 function showProductDescription() {
     let htmltoadd = "";
@@ -16,21 +20,17 @@ function showProductDescription() {
      <p><strong>Categoría: </strong>
      </br>${infProductList.category}</p>
      <p><strong>Cantidad de vendidos: </strong>
-     </br>${infProductList.soldCount}</p>` +
+     </br>${infProductList.soldCount}</p>`
 
 
-        //////////////////////////Imagenes////////////////
+    //////////////////////////////Recorrer imagens y mostrarlas////////////////////////////////////////////////
+    let imgs = "";
+    for (rutaImg of infProductList.images) {
+        imgs += `<a class="thumbnail" href="#thumb"><img src="${rutaImg}" width="23%" /><span><img src="${rutaImg}" ></span></a>`;
+        document.getElementById("imgs-Id").innerHTML = imgs;
+    }
 
-        `<span><strong>Imagenes ilustrativas</strong></span>
-    <div class="img-inf">
-    <a class="thumbnail" href="#thumb"><img src="${infProductList.images[0]}" width="23%" /><span><img src="${infProductList.images[0]}" ></span></a>
-    <a class="thumbnail" href="#thumb"><img src="${infProductList.images[1]}" width="23%" /><span><img src="${infProductList.images[1]}" ></span></a> 
-    <a class="thumbnail" href="#thumb"><img src="${infProductList.images[2]}" width="23%" /><span><img src="${infProductList.images[2]}" ></span></a> 
-    <a class="thumbnail" href="#thumb"><img src="${infProductList.images[3]}" width="23%" /><span><img src="${infProductList.images[3]}" ></span></a> 
-    </div></br></br></br>`
-
-
-    document.getElementById("InfoProd").innerHTML = htmltoadd;
+    document.getElementById("InfoProd").innerHTML += htmltoadd;
 
 }
 
@@ -44,22 +44,22 @@ function showProductDescription() {
 function showProductComents() {
     let agregarComentario = "";
 
-
     for (let i = 0; i < infProductComents.length; i++) {
         let coments = infProductComents[i];
-        console.log(coments.score);
+
+
 
         agregarComentario += `<li id="comentsList" list-style: none;><strong>${coments.user}</strong> ${coments.dateTime} ${showStars(coments.score)} 
         </br>${coments.description}</li>`
 
-        document.getElementById("comments").innerHTML = agregarComentario+newComent
+        document.getElementById("comments").innerHTML = agregarComentario //newComent
 
-        
+
+
+
     }
 
 }
-
-
 document.addEventListener("DOMContentLoaded", function (a) {
 
     getJSONData(PRODUCT_INFO_URL + infoP + EXT_TYPE).then(function (resultObj) {
@@ -76,14 +76,52 @@ document.addEventListener("DOMContentLoaded", function (a) {
 document.addEventListener("DOMContentLoaded", function (a) {
     getJSONData(PRODUCT_INFO_COMMENTS_URL + infoP + EXT_TYPE).then(function (resultObj) {
         if (resultObj.status === "ok") {
-            infProductComents = resultObj.data
+            infProductComents = resultObj.data;
+
+
+
+            let newCom = localStorage.getItem("Nuevo comentario")
+            newCom_obj = JSON.parse(newCom);
+            //////////////Condicional para que traiga el comentario con mismo id que el producto////////////////////////////////
+            //if (newCom_obj.product == parseInt(infoP)) {}
+            infProductComents.push(JSON.parse(newCom))
+            
             showProductComents(infProductComents);
             console.log(infProductComents);
-        }
 
+        }
     });
 
-})
+    console.log(localStorage.getItem("Nuevo comentario"));
+
+});
+/////////////////Escucha de evento para boton de enviar comentario///////////////////////
+document.getElementById("getCom").addEventListener("click", function () {
+
+    let nuevoDato = document.getElementById("comArea").value;
+    let rateNew = document.getElementById("getRate").value;
+    let today = new Date();
+    let now = today.toLocaleString("sv-SE");
+
+
+    let newComentary = {
+        product: parseInt(localStorage.getItem("ProdID")),
+        description: nuevoDato,
+        score: parseInt(rateNew),
+        user: localStorage.getItem("User"),
+        dateTime: now,
+    }
+
+    let newComentary_json = JSON.stringify(newComentary);
+    arrayComs += newComentary_json;
+
+
+    localStorage.setItem("Nuevo comentario", arrayComs);
+
+});
+
+
+
 ///////////////////Funcion para mostrar estrellas//////////////
 function showStars(n) {
     let okStars = "";
@@ -100,42 +138,5 @@ function showStars(n) {
     return okStars + nonStars;
 }
 
-
-let newComent = [];
-
-document.getElementById("getCom").addEventListener("click", function(){
-
-    let nuevoDato = document.getElementById("comArea").value;
-    let rateNew = document.getElementById("getRate").value;
-    let today = new Date();
-    let now = today.toLocaleString("sv-SE");
-
-    if(nuevoDato !==""){
-    newComent += `<li id="comentsList" list-style: none><strong>${localStorage.getItem("User")}</strong> ${now} ${showStars(rateNew)}
-    </br>${nuevoDato}</li>`;
-    }
-    document.getElementById("comments").innerHTML = newComent;
-
-    document.getElementById("comArea").value="";
-
-    let newComent_json = JSON.stringify(newComent);
-
-    localStorage.setItem("newCom", newComent_json);
-
-});
-
-//Recuperar datos.
-document.addEventListener("DOMContentLoaded", function(){
-
-    if(localStorage.getItem("newCom")){
-
-       newComent_json = localStorage.getItem("newCom");
-
-        newComent = JSON.parse(newComent_json);
-
-        document.getElementById("comments").innerHTML += newComent;
-    }
-    console.log(newComent);
-});
 
 
